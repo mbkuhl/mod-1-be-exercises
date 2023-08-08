deaec6141908fe0c7a2896da247516f95cabaf62
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-08-08 01:05:26 -0500

    Complete hobbit class

---
+++ ruby_exercises/mythical-creatures/lib/hobbit.rb
@@ -0,0 +1,34 @@
+class Hobbit
+    attr_reader :name, :disposition, :age
+    def initialize(name, disposition = "homebody")
+        @name = name
+        @disposition = disposition
+        @age = 0
+    end
+
+    def celebrate_birthday
+        @age += 1
+    end
+
+    def adult?
+        @age >= 33
+    end
+
+    def old?
+        @age >= 101
+    end
+
+    def has_ring?
+        if @name == 'Frodo'
+            true
+        else
+            false
+        end
+    end
+
+    def is_short?
+        true
+    end
+        
+
+end
